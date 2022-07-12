8fd938c177f56f42bc3ccf6c0c0dde6eb48ed475
JaredChen0317 <guitar0317@gmail.com>
2022-07-12 09:34:40 +0800

    Add linkedlist practice

---
+++ test/Program.cs
@@ -0,0 +1,27 @@
+﻿//Console.WriteLine("Hello, World!");
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 8, b = 2, c=3;
+        string s="a";
+        //Console.WriteLine("Hello, World!");
+        switch(s){
+            case "a":
+                if(a>b){
+                    if(a<c) break;
+                    else if(c<b) break;
+                    for(int i=0; i<10; i++){
+                         Console.WriteLine(i);
+                        if(i==b) break;
+                    }
+                }
+                Console.WriteLine("a");
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
